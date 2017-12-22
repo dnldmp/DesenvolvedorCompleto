58dbbc9a3062ba5f4449c28b683f207896da6cb5
danilo dominoni <danilodomp@gmail.com>
2017-12-22 13:57:46 -0200

    Criando input e botao, recuperando os valores com js e fazendo soma

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 /*
 alert("Olá HCODE!");
-var resposta = confirm("Deseja excluir?");
+const resposta = confirm("Deseja excluir?");
 console.log(resposta);
 */
 
-var a = 10;
-var b = 33;
+const a = 10;
+const b = 33;
 
 function somar(a, b){
-
-	return a + b;
-
+	return parseInt(a) + parseInt(b);
 }
 
-console.log(somar(2, 5));
+document.querySelector("#calcular").addEventListener("click", function(){
+	var valorA = document.querySelector("#valorA").value;
+	var valorB = document.querySelector("#valorB").value;
+	if(valorA.length > 0 && valorB.length > 0 ) {
+		//alert(parseInt(valorA) + parseInt(valorB));
+		somar(valorA, valorB);
+	} else {
+		alert("Digite os valores para o cálculo");
+	}
+});
